474dbe123cf7762067013947b4b23e5ced3135d6
silly pilly <165362601+Gonistical@users.noreply.github.com>
2024-06-21 10:02:19 +0300

    Create joinlogger.lua

---
+++ examples/joinlogger.lua
@@ -0,0 +1,35 @@
+local Webhook = ""
+local Players = game:GetService("Players")
+local HttpService = game:GetService("HttpService")
+
+local HookBlox = require(script.HookBlox)
+local EmbedBuilder = HookBlox.EmbedBuilder
+local WebhookClient = HookBlox.new(Webhook)
+
+local function LogPlayer(player: Player)
+	warn("hi guys")
+	warn("Player named "..player.Name.." has joined the experience.")
+
+	local JoinEmbed = EmbedBuilder.new()
+	JoinEmbed:setProperties({
+		title = "USER LOGGER",
+		url = "https://www.roblox.com/users/"..player.UserId.."/profile",
+	})
+
+	JoinEmbed:createField({
+		name = "👤 User",
+		value = "**"..player.DisplayName.."** (@"..player.Name..")",
+		inline = true
+	})
+
+	local response = WebhookClient:sendContent({
+		content = 'A new user has been logged!',
+		embeds = {JoinEmbed},
+		
+	})
+end
+
+for i,v in ipairs(Players:GetPlayers()) do
+	LogPlayer(v)
+end
+Players.PlayerAdded:Connect(LogPlayer)
