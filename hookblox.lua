406fce44d6e32868589362c5ad34802a2a6121d6
silly pilly <165362601+Gonistical@users.noreply.github.com>
2024-06-21 10:03:05 +0300

    Create hookblox.lua

---
+++ hookblox.lua
@@ -0,0 +1,142 @@
+--[[
+
+	------------------------------------------------------------------------- 
+	
+	$$\   $$\                     $$\       $$$$$$$\  $$\                     
+	$$ |  $$ |                    $$ |      $$  __$$\ $$ |                    
+	$$ |  $$ | $$$$$$\   $$$$$$\  $$ |  $$\ $$ |  $$ |$$ | $$$$$$\  $$\   $$\ 
+	$$$$$$$$ |$$  __$$\ $$  __$$\ $$ | $$  |$$$$$$$\ |$$ |$$  __$$\ \$$\ $$  |
+	$$  __$$ |$$ /  $$ |$$ /  $$ |$$$$$$  / $$  __$$\ $$ |$$ /  $$ | \$$$$  / 
+	$$ |  $$ |$$ |  $$ |$$ |  $$ |$$  _$$<  $$ |  $$ |$$ |$$ |  $$ | $$  $$<  
+	$$ |  $$ |\$$$$$$  |\$$$$$$  |$$ | \$$\ $$$$$$$  |$$ |\$$$$$$  |$$  /\$$\ 
+	\__|  \__| \______/  \______/ \__|  \__|\_______/ \__| \______/ \__/  \__|
+                                                                          
+    -------------------------------------------------------------------------  
+    
+	HookBlox.lua
+	A Module that makes webhooks as easy as possible!
+	
+	------------------------------------------------------------------------- 
+	
+	Featuring: Built-in Embed Builder
+	Created by @Gonistical / Date: 6/21/2024
+	
+	------------------------------------------------------------------------- 
+	 
+	DEVFORUM POST: https://devforum.roblox.com/
+	
+	------------------------------------------------------------------------- 
+--]]
+
+
+local hooks = {}
+hooks.__index = hooks
+
+local HTTP = game:GetService("HttpService")
+local types = require(script:FindFirstChild("Types"))
+local embedBuilder = script:FindFirstChild("EmbedBuilder")
+hooks.EmbedBuilder = require(embedBuilder)
+
+function hooks.new(webhook : string)
+	assert(webhook, 'No webhook was provided?')
+	assert(HTTP.HttpEnabled, 'HttpRequests are disabled!')
+	assert(type(webhook) == "string", 'The webhook URL must be a string.')
+	
+	local WebhookParameters = HTTP:RequestAsync({
+		Url = webhook,
+		Method = "GET"
+	})
+	local WebhookData = HTTP:JSONDecode(WebhookParameters["Body"])
+	if WebhookData.message then
+		assert(WebhookData.message ~= "Unknown Webhook", 'Webhook URL is invalid')
+	end
+	
+	local WebhookBody : types.webhookBody = {
+		guild = {
+			guildId = WebhookData.guild_id,
+			channelId = WebhookData.channel_id
+		},
+		name = WebhookData.name,
+		token = WebhookData.token,
+		webhookType = WebhookData["type"],
+		url = WebhookData.url,
+		id = WebhookData.id
+	}
+	
+	setmetatable(WebhookBody, hooks)
+	return WebhookBody
+end
+
+function hooks:delete()
+	local Deletion = HTTP:RequestAsync({
+		Method = "DELETE",
+		Url = self.url
+	})
+	self = {}
+end
+
+function hooks:sendJSON(json : {})
+	assert(json, 'No data was provided')
+	
+	local Response = HTTP:RequestAsync({
+		Url = self.url,
+		Method = "POST",
+		Body = json,
+		Headers = {
+			["Content-Type"] = "application/json"
+		}
+	})
+end
+
+function hooks:sendContent(content : types.webhookSendQuery | string?)
+	assert(content, 'No data was provided')
+	
+	local luaContent = {}
+	if type(content) == 'string' then
+		luaContent.content = content
+		local encoded = HTTP:JSONEncode(luaContent)
+		local Response = HTTP:RequestAsync({
+			Url = self.url,
+			Method = "POST",
+			Body = encoded,
+			Headers = {
+				["Content-Type"] = "application/json"
+			}
+		})
+		return
+	end
+	
+	local avatarURL = content.avatar
+	local username = content.name
+	local contentString = content.content
+	
+	luaContent.content = contentString
+	luaContent.username = username
+	luaContent.avatar_url = avatarURL
+	luaContent.thread_name = content.threadName
+	luaContent.embeds = content.embeds
+	
+	if content.flags then
+		if content.flags.suppressEmbeds and content.flags.suppressNotifs then
+			luaContent.flags = 4100
+		elseif content.flags.suppressEmbeds then
+			luaContent.flags = 4096
+		elseif content.flags.suppressNotifs then
+			luaContent.flags = 4
+		end
+	end
+	
+	local encoded = HTTP:JSONEncode(luaContent)
+	local Response = HTTP:RequestAsync({
+		Url = self.url,
+		Method = "POST",
+		Body = encoded,
+		Headers = {
+			["Content-Type"] = "application/json"
+		}
+	})
+	
+	return Response
+end
+
+return hooks
