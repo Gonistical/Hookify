2c5b0bc65f11df181d1c1d8796f0c2581f94bc01
silly pilly <165362601+Gonistical@users.noreply.github.com>
2024-06-21 10:03:55 +0300

    Create EmbedBuilder.lua

---
+++ src/EmbedBuilder.lua
@@ -0,0 +1,55 @@
+local builder = {}
+local types = require(script.Parent.Types)
+builder.__index = builder
+
+function builder.new()
+	local newEmbed = {}
+	setmetatable(newEmbed, builder)
+	newEmbed.fields = {}
+	return newEmbed
+end
+
+function builder:setProperties(properties: types.embedProperties)
+	local content = self
+	if not properties.color then
+		properties.color = Color3.fromRGB(0,0,0):ToHex()
+	end
+	
+	self.title = properties.title
+	self.description = properties.description
+	self.url = properties.url
+	self.color = properties.color
+end
+
+function builder:setTimestamp(timestamp: string)
+	self.timestamp = timestamp
+end
+
+function builder:setImages(imageProperties: types.imageProperties)
+	if imageProperties.image then
+		self.image.url = imageProperties.image
+	end
+	
+	if imageProperties.thumbnail then
+		self.thumbnail.url = imageProperties.thumbnail
+	end
+end
+
+function builder:setAuthor(author: types.author)
+	self.author = {
+		name = author.name;
+		url = author.url;
+		icon_url = author.imageUrl;
+	}
+end
+
+function builder:createField(fieldProperties : types.fieldProperties)
+	local field = {
+		name = fieldProperties.name,
+		value = fieldProperties.value,
+		inline = fieldProperties.inline
+	}
+	table.insert(self.fields, field)
+end
+
+return builder
